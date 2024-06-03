0cdf07b8d6a5f9d42ca87e5851776426adbf67b8
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-06-03 00:25:41 -0800

    updated projects

--- src/componets/Portfolio/Portfolio.jsx
+++ src/componets/Portfolio/Portfolio.jsx
@@ -7,16 +7,16 @@ import { useRef } from 'react';
 const items=[
     {
         id:1,
-        title:'Pexels Clone ',
-        img:"/pexels.jpg",
-        desc:`In developing my Pexels clone, I honed my skills in HTML, CSS, and JavaScript, showcasing my proficiency in front-end web development. This project allowed me to master the art of designing visually appealing and responsive user interfaces, demonstrating my understanding of layout structuring, styling elements, and implementing interactive features. By incorporating JavaScript, I enhanced the user experience through dynamic functionalities, showcasing my ability to manipulate the Document Object Model (DOM) seamlessly. This project not only highlights my technical competence but also reflects my dedication to creating user-friendly and aesthetically pleasing web applications, making it a valuable addition to my portfolio`,
-        link:" https://shashankpandey09.github.io/pexels-clone/",
+        title:'Netflix-Gpt',
+        img:"https://www.missioncriticalmagazine.com/ext/resources/2023/10/CHatgpt.jpg?1697059739",
+        desc:`I developed a Netflix UI clone featuring a sleek, responsive design built with React.js and styled using Tailwind CSS. The project includes secure backend authentication using Firebase, enabling user registration, login, and logout functionalities. Additionally, I integrated OpenAI's GPT-3 for an advanced search feature that provides intelligent content recommendations based on user queries. Key technologies used are React.js for the frontend, Firebase for authentication, and the GPT-3 API for the search functionality. This project showcases my ability to create dynamic user interfaces, implement secure authentication, and enhance user experience with AI-powered features.`,
+        link:" https://netflix-gpt-ashy-three.vercel.app/"
     },
     {
         id:2,
-        title:'React News App',
-        img:"/pexels-news.jpg",
-        desc:"Through the development of my React-based news website, I elevated my skills in modern web development. By implementing API fetching, I mastered the integration of dynamic content, showcasing my ability to create real-time updates and maintain a seamless user experience. This project underscores my proficiency in React, demonstrating my understanding of component-based architecture, state management, and the importance of responsive design. Beyond technical prowess, it reflects my commitment to delivering relevant and engaging content to users, making it a compelling showcase of my capabilities in both front-end development and user-centric design for my portfolio.",
+        title:'E-commerce Website',
+        img:"https://www.hostgator.com/blog/wp-content/uploads/2020/08/Types-of-eCommerce-Websites-1024x538.jpg",
+        desc:"I developed an e-commerce website using React and Redux for state management, creating a dynamic and responsive user interface. The project features secure backend authentication using Firebase, allowing users to register, log in, and log out seamlessly. Key technologies include React.js for the frontend, Redux for efficient state management, and Firebase for authentication. This project demonstrates my proficiency in building scalable web applications, implementing robust user authentication, and managing complex state with Redux.",
         link:"https://shashankpandey09.github.io/unityLab/"
     },
     {
